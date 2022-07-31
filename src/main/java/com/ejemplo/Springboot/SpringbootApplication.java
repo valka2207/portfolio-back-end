cab70870cc8867a6a5034a0927ea2c1745595842
anahi <chesterygandalf@gmail.com>
2022-07-30 21:17:45 -0300

    firebase

--- src/main/java/com/ejemplo/Springboot/SpringbootApplication.java
+++ src/main/java/com/ejemplo/Springboot/SpringbootApplication.java
@@ -17,7 +17,7 @@ public class SpringbootApplication {
             return new WebMvcConfigurer(){
                 @Override
                 public void addCorsMappings(CorsRegistry registry){
-                    registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS");
+                    registry.addMapping("/**").allowedOrigins("https://acarranza-89cf3.web.app/").allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS");
                 }
             };
         }
